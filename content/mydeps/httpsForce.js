29b1aaaa13c02927dec7a5e21cd51d6b2bc6b7a6
MrShoenel <development@hoenel.net>
2015-12-31 17:10:22 +0100

    Compress everything

--- content/mydeps/httpsForce.js
+++ content/mydeps/httpsForce.js
@@ -1,8 +1,1 @@
-~function() {
-	if (location.host !== 'mrshoenel.github.io') {
-		return; // only run on mrshoenel.github.io
-	}
-	if (location.protocol === 'http:') {
-		location.replace('https://' + location.host + location.pathname + location.search + location.hash, true);
-	}
-}();
+~function(){"mrshoenel.github.io"===location.host&&"http:"===location.protocol&&location.replace("https://"+location.host+location.pathname+location.search+location.hash,!0)}();
