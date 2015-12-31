784e8d9760fe82148adcb414bb5d232e5b7a5eb8
MrShoenel <development@hoenel.net>
2015-12-31 16:41:14 +0100

    Remove content that shouldn't have been there

--- script/content/mydeps/httpsForce.js
+++
@@ -1,8 +0,0 @@
-~function() {
-	if (location.host !== 'mrshoenel.github.io') {
-		return; // only run on mrshoenel.github.io
-	}
-	if (location.protocol === 'http:') {
-		location.replace('https://' + location.host + location.pathname + location.search + location.hash, true);
-	}
-}();
--- script/content/mydeps/mydate.directive.js
+++
@@ -1,6 +0,0 @@
-angular.module('blogapp').filter('mrshMydate', function() {
-	var dateMethod = Date.prototype['toGMTString'] || Date.prototype.toLocaleString || Date.prototype.toString;
-	return function(input) {
-		return dateMethod.call(new Date(Date.parse(input)));
-	};
-});
