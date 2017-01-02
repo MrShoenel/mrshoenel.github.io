667b10a1b352a1516dc3d381f76c88f8856bc8e2
MrShoenel <development@hoenel.net>
2017-01-02 01:10:03 +0100

    Update the youtube-wrapper script to check for 10 seconds

--- content/mydeps/youtubeWraps.js
+++ content/mydeps/youtubeWraps.js
@@ -1,17 +1,26 @@
-window.setTimeout(() => {
-	var divs = document.querySelectorAll('div[role="youtube-wrapper"]');
-	for (var i = 0; i < divs.length; i++) {
-		var wrap = divs.item(i),
-			id = divs.item(i).getAttribute('data-for-id'),
-			w = wrap.parentElement.offsetWidth,
-			ifr = document.createElement('iframe');
-		
-		ifr.setAttribute('src', 'https://www.youtube.com/embed/' + id);
-		ifr.setAttribute('width', w);
-		ifr.setAttribute('height', (w / 16 * 9) | 0);
-		ifr.setAttribute('frameborder', '0');
-		ifr.setAttribute('allowfullscreen', '');
+(() => {
+	var tries = 10;
+	var interval = window.setInterval(() => {
+		var divs = document.querySelectorAll('div[role="youtube-wrapper"]');
+		for (var i = 0; i < divs.length; i++) {
+			var wrap = divs.item(i),
+				id = divs.item(i).getAttribute('data-for-id'),
+				w = wrap.parentElement.offsetWidth,
+				ifr = document.createElement('iframe');
+			
+			ifr.setAttribute('src', 'https://www.youtube.com/embed/' + id);
+			ifr.setAttribute('width', w);
+			ifr.setAttribute('height', (w / 16 * 9) | 0);
+			ifr.setAttribute('frameborder', '0');
+			ifr.setAttribute('allowfullscreen', '');
 
-		wrap.appendChild(ifr);
-	}
-}, 250);
+			wrap.appendChild(ifr);
+			wrap.removeAttribute('role');
+		}
+
+		tries--;
+		if (tries === 0) {
+			window.clearInterval(interval);
+		}
+	}, 1000);
+})();
